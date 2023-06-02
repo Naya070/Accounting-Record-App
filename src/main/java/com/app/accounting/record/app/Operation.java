b0a0182fa4a9d9b1beb2148bbcc8232218eb2074
Naya070 <nayarith070@gmail.com>
2023-06-02 12:58:25 -0400

    secrearon clases DAO

--- src/main/java/com/app/accounting/record/app/Operation.java
+++ src/main/java/com/app/accounting/record/app/Operation.java
@@ -9,5 +9,49 @@ package com.app.accounting.record.app;
  * @author naya
  */
 public class Operation {
+    int id_operation;
+    float amount;
+    String created_at;
+    String description;
+
+    public Operation(float amount, String created_at, String description) {
+        this.amount = amount;
+        this.created_at = created_at;
+        this.description = description;
+    }
+    public Operation () {}
+
+    public int getId_operation() {
+        return id_operation;
+    }
+
+    public void setId_operation(int id_operation) {
+        this.id_operation = id_operation;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
     
 }
--- src/main/java/com/app/accounting/record/app/UserLog.java
+++ src/main/java/com/app/accounting/record/app/UserLog.java
@@ -9,5 +9,73 @@ package com.app.accounting.record.app;
  * @author naya
  */
 public class UserLog {
+    int idUserLog;
+    String name;
+    String lastname;
+    String username;
+    String email;
+    String password;
+
+    public UserLog(String name, String lastname, String username, String email, String password) {
+        this.name = name;
+        this.lastname = lastname;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+    public UserLog(){}
+
+    public int getIdUserLog() {
+        return idUserLog;
+    }
+
+    public void setIdUserLog(int idUserLog) {
+        this.idUserLog = idUserLog;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+    
+    
     
 }
